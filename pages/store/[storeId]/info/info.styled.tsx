dec6d7b1901be5d4bbec06d65683945a5ccc35ed
rachel490 <ccori12@naver.com>
2023-02-20 19:55:49 +0900

    test2

--- pages/store/[storeId]/info/info.styled.tsx
+++
@@ -1,5 +0,0 @@
-import styled from "@emotion/styled";
-
-export const Container = styled.div`
-  padding-bottom: 4.5rem;
-`;
--- pages/store/[storeId]/menu/menu.styled.ts
+++
@@ -1,9 +0,0 @@
-import styled from "@emotion/styled";
-
-export const Container = styled.div`
-  padding-bottom: 4.5rem;
-`;
-
-export const ListWrap = styled.div`
-  padding: 3.2rem 1.6rem;
-`;
